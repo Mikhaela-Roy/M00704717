b32a8e59927c5de91f859ce403cc61ec8d47fe9f
Mikhaela-Roy <97114468+Mikhaela-Roy@users.noreply.github.com>
2022-01-27 14:12:36 +0000

    Eye animation
    Improvement on my eye and added a special feature.

--- Animation_2.py
+++ Animation_2.py
@@ -1,15 +1,18 @@
 import opc
 from time import *
 import random
+import colorsys
 
 leds = [(0,0,0)]*360
 
 client = opc.Client('localhost:7890')
 client.put_pixels(leds)
 
+
 #rand_colour = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
 
 #references of position of LEDs
+
 ##row1 = [25, 28, 29, 30, 31, 34]
 ##row2 = [83, 87, 88, 89, 90, 91, 92, 97]
 ##row3 = [141, 147,148, 149, 150, 151, 152, 162]
@@ -17,39 +20,39 @@ client.put_pixels(leds)
 ##row5 = [261, 267, 268, 269, 270, 271, 272, 282]
 ##row6 = [325, 328, 329, 330, 331, 338]
 
+
 def eye():
-    fade_amount = 5
+    s = 1.0 ##maximum colour
+    v = 1.0 ##maximum brightness
+
     
     for i in range(0,360): #Run from 0 to 359
+        rgb_fractional = colorsys.hsv_to_rgb(i/360, s, v) #colorsys returns floats between 0 and 1
+        r_float = rgb_fractional[0] #extract said floating point numbers
+        g_float = rgb_fractional[1]
+        b_float = rgb_fractional[2]
+
+        rgb = (r_float*255, g_float*255, b_float*255) #make new tuple with corrected values
+        
         if i == 360 or i == 0: #beginning random colour and when loop ends restart colour
-                rand_colour = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
-        for led in enumerate(rand_colour):
-            if i >= 25 and i <=34 or i >= 81 and i <= 98 or i >= 139 and i <= 160 or i >= 197 and i <= 222 or i >= 261 and i <= 278 or i >= 325 and i <= 334:
-                leds[i] = (255,255,255) #when the range is between the values listed above change the colour to white
-                client.put_pixels(leds)
-                sleep(0.1)
-                
-            if i >= 28 and i <= 31 or i >= 87 and i <= 92 or i >= 147 and i <= 152 or i >= 207 and i <= 212 or i >= 267 and i <= 272 or i >= 328 and i <= 331:
-                leds[i] = rand_colour #when the range is between the values listed above change colour to the initial random generated colour
-                client.put_pixels(leds)
-                sleep(0.01)
-                
-                r,g,b = leds[1]
-                r = r+fade_amount
-                g = g+fade_amount
-                b = b+fade_amount
-
-                new_colour = (r,g,b)
-                leds[led[0]] = new_colour
-
-                if r >= 359 or r > 0:
-                    fade_amount = -fade_amount
-                    leds[i] = new_colour #when the range is between the values listed above change colour to the initial random generated colour
-                    client.put_pixels(leds)
-                    sleep(0.1)
+            rand_colour = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
     
+        if i >= 25 and i <=34 or i >= 81 and i <= 98 or i >= 139 and i <= 160 or i >= 197 and i <= 222 or i >= 261 and i <= 278 or i >= 325 and i <= 334:
+            leds[i] = (255,255,255) #when the range is between the values listed above change the colour to white
+            client.put_pixels(leds)
+            sleep(0.1)
+            
+       
+        if i >= 28 and i <= 31 or i >= 87 and i <= 92 or i >= 147 and i <= 152 or i >= 207 and i <= 212 or i >= 267 and i <= 272 or i >= 328 and i <= 331:
+            leds[i] = rand_colour #when the range is between the values listed above change colour to the initial random generated colour
+            
+            client.put_pixels(leds)
+            sleep(0.01)
+                
+            client.put_pixels([rgb]*360)
+            sleep(0.1)
+        
     #put in one if statements
     #get the put pixels after if statements
 
-for i in range(2):
-    eye()
+
--- Animation_3.py
+++ Animation_3.py
@@ -1,24 +1,33 @@
 import opc
-from time import sleep
+from time import *
+import random
+from matplotlib import pyplot as plt
+import numpy as ass
+import pylab
+import scipy
 
-client = opc.Client('localhost:7890')
+x = ass.linspace(2,6,1000)
+y1 = ass.sqrt(1+(abs(x)+1)**2)
+y2 = 3*ass.sqrt(1+(abs(x)/2)**0.5)
+pylab.fill_between(x, y1, color='red')
+pylab.fill_between(x, y2, color='red')
+#pylab.xlim([-2.5, 2.5])
+#pylab.text(0, -0.4, 'Stack Overflow', fontsize=24, fontweight='bold#color='white', horizontalalignment='center')
+pylab.savefig('heart.png')
 
-led_list = [(0,0,0)]*360
-fade_amount = 5
-
-while True:
-    for led in enumerate(led_list):
-
-        r,g,b = led[1]
-        r = r+fade_amount
-        g = g+fade_amount
-        g = b+fade_amount
-
-        new_colour = (r,g,b)
-        led_list[led[0]] = new_colour
-
-        if r >= 255 or r <= 0:
-            fade_amount = -fade_amount
-
-    client.put_pixels(led_list)
-    sleep(0.2)
+##leds = [(0,0,0)]*360
+##
+##client = opc.Client('localhost:7890')
+##client.put_pixels(leds)
+##
+###Creating equally spaced 100 data in range 0 to 2*pi
+##theta = ass.linspace(0,2*ass.pi,100) 
+##
+###Generating x and y data
+##x = 6*(ass.sin(theta)**2) 
+##y = 3 * ass.cos(theta) - 5* ass.cos(2*theta) - 2 * ass.cos(3*theta) - ass.cos(4*theta)
+##
+###Plotting
+##plt.plot(x,y)
+##plt.title('Heart Shape')
+##plt.show()
