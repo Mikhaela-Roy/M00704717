d653ca042f10f8e4e57dfd6c753e4bdfbe17b13e
Mikhaela-Roy <97114468+Mikhaela-Roy@users.noreply.github.com>
2022-02-01 14:25:37 +0000

    Changes
    The changes I have made was having the eye animation flashing whilst also scrolling. I have also created a heart animation manually but not have added the features as of yet.

--- Animation_2.py
+++ Animation_2.py
@@ -20,30 +20,32 @@ client.put_pixels(leds)
 ##row6 = [325, 328, 329, 330, 331, 338]
 
 def eye():
+    
     s = 1.0 ##maximum colour
     v = 1.0 ##maximum brightness
-    
+
     for i in range(0,360): #Run from 0 to 359
-        rgb_fractional = colorsys.hsv_to_rgb(i/360, s, v) #colorsys returns floats between 0 and 1
+        rgb_fractional = colorsys.hsv_to_rgb(i/360.0, s, v) #colorsys returns floats between 0 and 1
         r_float = rgb_fractional[0] #extract said floating point numbers
         g_float = rgb_fractional[1]
         b_float = rgb_fractional[2]
 
         rgb = (r_float*255, g_float*255, b_float*255) #make new tuple with corrected values
         
+
         if i == 360 or i == 0: #beginning random colour and when loop ends restart colour
-            rand_colour = (random.randint(0,255),random.randint(0,255),random.randint(0,255)) #random colour 
-    
+            rand_colour = (random.randint(0,255),random.randint(0,255),random.randint(0,255)) #random colour
+            
         if i >= 25 and i <=34 or i >= 81 and i <= 98 or i >= 139 and i <= 160 or i >= 197 and i <= 222 or i >= 261 and i <= 278 or i >= 325 and i <= 334:
             leds[i] = (255,255,255) #when the range is between the values listed above change the colour to white
-            client.put_pixels(leds)
-            sleep(0.1)
-       
+            
         if i >= 28 and i <= 31 or i >= 87 and i <= 92 or i >= 147 and i <= 152 or i >= 207 and i <= 212 or i >= 267 and i <= 272 or i >= 328 and i <= 331:
             leds[i] = rand_colour #when the range is between the values listed above change colour to the initial random generated colour
             client.put_pixels(leds)
-            sleep(0.01)
-            client.put_pixels([rgb]*360) #sends out
             sleep(0.1)
-
-eye()         
+            client.put_pixels([rgb]*1) #send out
+            sleep(0.01)
+                              
+        client.put_pixels(leds)
+        sleep(0.1)
+eye()          
--- Final.py
+++ Final.py
@@ -1,6 +1,8 @@
 from tkinter import *
 import Animation_1
 import Animation_2
+import Animation_3
+import Animation_4
 
 root = Tk()
 root.title('Animation')
@@ -10,5 +12,7 @@ User = Label(root, text = "Choose the animation you wish to see").grid(row = 1, 
 
 Snake = Button(root, text = 'Snake Animation', command = Animation_1.snake).grid(row = 2, column = 0)
 Eye = Button(root, text = 'Eye Animation', command = Animation_2.eye).grid(row = 3, column = 0)
+TTT = Button(root, text = 'TicTacToe Animation', command = Animation_3).grid(row = 4, column = 0)
+Heart = Button(root, text = 'Heart Animation', command = Animation_4.heart_shape).grid(row = 5, column = 0)
 
 root.mainloop()
