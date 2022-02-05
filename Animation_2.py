95525f08bc606f77dad0dc19553063453500bdca
Mikhaela-Roy <97114468+Mikhaela-Roy@users.noreply.github.com>
2022-02-05 14:57:36 +0000

    Update on Eye Animation
    Eye animation is able to blink and rebuild then iris is able to flash random colors. Prompt to ask user if they would like to repeat animation if yes repeat, if no go to Snake Animtaion.

--- Animation_2.py
+++ Animation_2.py
@@ -16,48 +16,12 @@ client.put_pixels(leds)
 
 ##row1 = [25, 28, 29, 30, 31, 34]
 ##row2 = [83, 87, 88, 89, 90, 91, 92, 97]
-##row3 = [141, 147,148, 149, 150, 151, 152, 162]
+row3 = [141, 147,148, 149, 150, 151, 152, 162]
 ##row4 = [197, 207,208, 209, 210, 211, 212, 226]
 ##row5 = [261, 267, 268, 269, 270, 271, 272, 282]
 ##row6 = [325, 328, 329, 330, 331, 338]
 
 iris = [[28,29,30,31],[87,88,89,90,91,92],[147,148,149,150,151,152],[207,208,209,210,211,212],[267,268,269,270,271,272],[328,329,330,331]]
-
-def white():
-
-    rand_colour = (random.randint(0,255),random.randint(0,255),random.randint(0,255)) #random colour
-
-    inc = 0
-    q = 0
-    
-    for i in range(3):
-        print('i',i)
-        for loop in range(0,61):
-            print('loop',loop)
-            q += 1
-            if loop == 60:
-                inc += 1
-                sleep(1)
-                
-            c = loop + 60*inc
-            f = loop + (300-60*inc)
-            
-            print('f',f)
-            print('c',c)
-            print('inc',inc)
-            
-            if c >= 25 and c <=34 or c >= 81 and c <= 98 or c >= 139 and c <= 160 or f >= 197 and f <= 222 or f >= 261 and f <= 278 or f >= 325 and f <= 334:
-                leds[c-60] = (0,0,0)
-                client.put_pixels(leds)
-                leds[c] = (255,255,255)
-                client.put_pixels(leds)
-                leds[f] = (255,255,255)
-                client.put_pixels(leds)
-                #sleep(0.1)
-                leds[f+60] = (0,0,0)
-                client.put_pixels(leds)
-            
-white()
 
 def eye():
     
@@ -81,7 +45,6 @@ def eye():
             if i >= 25 and i <=34 or i >= 81 and i <= 98 or i >= 139 and i <= 160 or i >= 197 and i <= 222 or i >= 261 and i <= 278 or i >= 325 and i <= 334:
                 leds[i] = (255,255,255) #when the range is between the values listed above change the colour to white
                 client.put_pixels(leds)
-                #sleep(0.1)
             
         for x in iris:
             for y in x:
@@ -91,8 +54,58 @@ def eye():
             
         client.put_pixels(leds)
         sleep(0.1)
-    
         
-    #Animation_1.snake()
+    question = input('Would you like to repeat? ')
+    
+    if question == 'yes' or question == 'Yes' or question == 'Y':
+        white()
+    else:
+        Animation_1.snake()
+    
+def build():
+    
+    q = 0
+    
+    for i in range(3):          
+        for once in range(0,61):
+            if once == 60:
+                q += 1
+                sleep(1)
 
-#eye()
+            g = once + 60*q
+            h = once + (300-60*q)
+            
+            if g >= 25 and g <=34 or g >= 81 and g <= 98 or g >= 139 and g <= 160 or h >= 197 and h <= 222 or h >= 261 and h <= 278 or h >= 325 and h <= 334:
+                leds[g] = (255,255,255)
+                client.put_pixels(leds)
+                leds[h] = (255,255,255)
+                client.put_pixels(leds)
+        
+def white():
+
+    inc = 0
+    
+    for i in range(3):
+        for loop in range(0,61):
+            if loop == 60:
+                inc += 1
+                sleep(1)
+                
+            c = loop + 60*inc
+            f = loop + (300-60*inc)
+            
+            if c >= 25 and c <=34 or c >= 81 and c <= 98 or c >= 139 and c <= 160 or f >= 197 and f <= 222 or f >= 261 and f <= 278 or f >= 325 and f <= 334:
+                leds[c-60] = (0,0,0)
+                client.put_pixels(leds)
+                leds[c] = (255,255,255)
+                client.put_pixels(leds)
+                leds[f-60] = (255,255,255)
+                client.put_pixels(leds)
+                leds[f] = (0,0,0)
+                client.put_pixels(leds)
+                
+    build()
+    eye()
+  
+white()
+
