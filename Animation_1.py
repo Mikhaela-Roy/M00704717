7f277419b13bba1d9c8c6d7f9876c78f30fb6673
Mikhaela-Roy <97114468+Mikhaela-Roy@users.noreply.github.com>
2022-02-12 14:33:02 +0000

    Fixed Iris

--- Animation_1.py
+++ Animation_1.py
@@ -8,53 +8,66 @@ leds = [(0,0,0)]*360
 
 client = opc.Client('localhost:7890')
 client.put_pixels(leds)
-client.put_pixels(leds)
 
 def confirm():
     answer = messagebox.askquestion(title = 'Confirmation', message  = 'Do you wish to continue the Snake Animation? ')
     if answer == 'yes':
-        body = -5
-        snake()
+        leds[body] = (0,0,0)
+        client.put_pixels(leds)
+        snake(food_item())
         
 def food_item():
 
     food = []
+    l_colour = []
 
     for i in range(0,360,60):
         food.append(random.randint(i,i+60))
     for x in food:
         leds[x] = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
+        colour = list(leds[x])
+        l_colour.append(colour)
+
 
     client.put_pixels(leds)
     client.put_pixels(leds)
     sleep(0.1)
 
-    return food
+    return food,l_colour
+
+#food_item()
     
 def snake():
-    
-    body = 0
-    
-    while body < 355: # when body is
-        
-        leds[body] = (200,100,255) #5 led is created
-        leds[body+1] = (0,0,0) #background back to default
-        
-        if body >= 354: #body is at the maximum led reset the value back to 0
-            confirm()
 
-        leds[body - 5] = (0,0,0) #change the led from the back to default
-
+    food,colour = food_item()
+    
+    body = 2
+    initial = (200,100,255)
+    
+    for led in range (0,360):
+        for seg in range(body):
+            leds[led+seg] = initial
+            if led > 0:
+                leds[led-1] = (0,0,0)
+                
         client.put_pixels(leds)
         sleep(0.1)
-        body += 1
+        
+        if led in food:
+            food.append(led)
+            for i in colour:
+                change = tuple(i)
+                if initial != i:
+                    initial = change
+                    leds[led+seg] = initial
+                    client.put_pixels(leds)  
+                    leds[led] = (0,0,0)
 
-
-        for y in food:
-            food.append(y)
-            colour = leds[y]
-            leds[y] = (0,0,0)
+            body += 1
             client.put_pixels(leds)
+            sleep(0.01)
+
+        if led >= 349:
+            confirm()
         
-        
-#snake(food_item())
+snake()
--- Animation_2.py
+++ Animation_2.py
@@ -21,40 +21,34 @@ client.put_pixels(leds)
 ##row5 = [261, 267, 268, 269, 270, 271, 272, 282]
 ##row6 = [325, 328, 329, 330, 331, 338]
 
-iris = [[28,29,30,31],[87,88,89,90,91,92],[147,148,149,150,151,152],[207,208,209,210,211,212],[267,268,269,270,271,272],[328,329,330,331]]
+iris = [28,29,30,31,87,88,89,90,91,92,147,148,149,150,151,152,207,208,209,210,211,212,267,268,269,270,271,272,328,329,330,331]
 
 def eye():
     
     messagebox.showwarning(title = '', message  = 'FLASH WARNING')
     
     for loop in range(5):    
-        s = 1.0 ##maximum colour
-        v = 1.0 ##maximum brightness
-
         for i in range(0,360): #Run from 0 to 359
-            rgb_fractional = colorsys.hsv_to_rgb(i/360.0, s, v) #colorsys returns floats between 0 and 1
-            r_float = rgb_fractional[0] #extract said floating point numbers
-            g_float = rgb_fractional[1]
-            b_float = rgb_fractional[2]
-
-            rgb = (r_float*255, g_float*255, b_float*255) #make new tuple with corrected values
             
             if i == 360 or i == 0: #beginning random colour and when loop ends restart colour
                 rand_colour = (random.randint(0,255),random.randint(0,255),random.randint(0,255)) #random colour
-                
+                 
             if i >= 25 and i <=34 or i >= 81 and i <= 98 or i >= 139 and i <= 160 or i >= 197 and i <= 222 or i >= 261 and i <= 278 or i >= 325 and i <= 334:
                 leds[i] = (255,255,255) #when the range is between the values listed above change the colour to white
                 client.put_pixels(leds)
+                #sleep(0.1)
+
             
         for x in iris:
-            for y in x:
-                leds[y] = rand_colour #when the range is between the values listed above change colour to the initial random generated colour
-                client.put_pixels([rgb]*1) #send out
-                sleep(0.01)
-            
-        client.put_pixels(leds)
-        sleep(0.02)
-        
+            leds[x] = rand_colour #when the range is between the values listed above change colour to the initial random generated colour
+            client.put_pixels(leds)
+            #sleep(0.02)
+
+        for y in iris:
+            leds[i] = (0,0,0)
+            client.put_pixels(leds)
+            sleep(0.02)
+         
     blink()
     
 def build():
@@ -79,31 +73,33 @@ def build():
 
 def blink():
 
-    d = 0
-    j = 2
+    inc = 0
+    
     for i in range(4):
-        for once in range(0,61):
+        for once in range(0,60):
             if once == 60:
-                d += 1
-                j -= 1
                 sleep(1)
                 
-            c = once + 60*d
-            f = once + (300-60*d)
+            c = once + 60*inc
+            f = once + (300-60*inc)
                 
             if c >= 25 and c <=34 or c >= 81 and c <= 98 or c >= 139 and c <= 160 or f >= 197 and f <= 222 or f >= 261 and f <= 278 or f >= 325 and f <= 334:
-                leds[c-60] = (0,0,0)
-                #client.put_pixels(leds)
                 leds[c] = (255,255,255)
-                #client.put_pixels(leds)
-                leds[f] = (0,0,0)
-                #client.put_pixels(leds)
-                leds[f-60] = (255,255,255)
+                client.put_pixels(leds)
+                leds[c-60] = (0,0,0)
+                client.put_pixels(leds)
+                leds[f] = (255,255,255)
+                client.put_pixels(leds)
+                leds[f-60] = (0,0,0)
+                client.put_pixels(leds)
                 
-    client.put_pixels(leds)
+                
 
                         
-    question = input('Would you like to repeat? ')
-    
-    if question == 'yes' or question == 'Yes' or question == 'Y':
-        build()
+##    question = input('Would you like to repeat? ')
+##    
+##    if question == 'yes' or question == 'Yes' or question == 'Y':
+##        build()
+        
+blink()
+
